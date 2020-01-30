bd35b746e5a1ea028874713671700e2d7e0f9182
TierraStover <tierrastover2002@yahoo.com>
2020-01-30 15:49:37 -0500

    added assignment 1

---
+++ assignmentOne/assignmentOne/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string firstName;
+		string middleInitial;
+		string lastName;
+		string fullName;
+
+		int age;
+		int heightFeet;
+
+		bool isCitizen;
+		bool canVote;
+
+		double heightInches;
+		double totalHeightCM;
+
+		Console.Write("Please Enter Your First Name: ");
+		firstName = Console.ReadLine();
+
+		Console.Write("Please Enter Your Middle Initial:");
+		middleInitial = Console.ReadLine();
+
+		Console.Write("Please Enter Your Last Name:");
+		lastName = Console.ReadLine();
+
+		Console.Write("\nPlease Enter Your Height In Feet (Ex. 3, 4, 5, 6, or 7):");
+		heightFeet = int.Parse(Console.ReadLine());
+
+		Console.Write("You are" + " " + heightFeet + " Feet" + " " + "and How Many Inches (Ex. 0, 1, 2.25, 3.50 or 4.75):");
+		heightInches = double.Parse(Console.ReadLine());
+
+		totalHeightCM = (((heightFeet * 12) + heightInches) * 2.54);
+
+		fullName = firstName + " " + middleInitial + " " + lastName;
+
+		Console.Write("\nAre you an US Citizen?(Yes/No):");
+		isCitizen = Console.ReadLine().ToUpper().StartsWith("Y");
+
+		Console.Write("Please Enter Your Age:");
+		age = int.Parse(Console.ReadLine());
+
+		if (age >= 18 && isCitizen == true)
+		{
+			canVote = true;
+		}
+		else
+		{
+			canVote = false;
+		}
+		Console.WriteLine("\n\n\t\t\t" + fullName + "'s Results:");
+
+		Console.WriteLine("\nYour Height of " + heightFeet + " Feet and " + heightInches + " Inches is equal to " + totalHeightCM + " Centimeters");
+
+		Console.WriteLine("You Are Eligible To Vote:" + " " + canVote);
+
+		Console.WriteLine("\n\n\t\tPress Any Key To Close The Window");
+		Console.ReadKey();
+
+	}
+}
