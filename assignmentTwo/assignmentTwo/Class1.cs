311619b951b9d8af5573d650f021f3e810de05f4
TierraStover <tierrastover2002@yahoo.com>
2020-03-04 18:25:02 -0500

    assignmentTwo

---
+++ assignmentTwo/assignmentTwo/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentTwo
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public static double SumOfAllAges;
+
+        public string GetFullName()
+        { 
+          
+        }
+
+        public void PrintNameAndAge()
+        {
+
+        }
+    }
+}
