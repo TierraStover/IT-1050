2049189c5433a0faff76cce9a0a2094f19e5087f
TierraStover <tierrastover2002@yahoo.com>
2020-03-19 11:12:25 -0400

    created classes and methods

---
+++ assignmentThree/assignmentThree/Instructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentThree
+{
+    class Instructor
+    {
+        private string Name;
+        private string CourseName;
+        
+        public void SetStudentGrade()
+        {
+
+        }
+    }
+}
---
+++ assignmentThree/assignmentThree/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentThree
+{
+    class Student
+    {
+        private string Name;
+        private int Grade;
+        private Instructor Teacher;
+
+        public void SetGrade()
+        {
+
+        }
+    }
+}
