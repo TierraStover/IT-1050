555ebc29df6d6e6c4fcf9f1c895c2fdf16e0b405
TierraStover <tierrastover2002@yahoo.com>
2020-04-13 00:37:20 -0400

    added promotions

--- assignmentFour/assignmentFour/Program.cs
+++ assignmentFour/assignmentFour/Program.cs
@@ -10,10 +10,15 @@ namespace assignmentFour
     {
         static void Main(string[] args)
         {
-         
+            Console.WriteLine(" \t\t\t*** DISCOUNTS AND PROMOTIONS ***");
+            Console.WriteLine(" \n\t 1) Each Purchase Of A Popcorn And A Large Drink");
+            Console.WriteLine(" \t\t Earns $2 OFF Of One Movie Ticket.");
+            Console.WriteLine(" \n\t 2)If 3 OR More People Purchase Tickets To An Evening Show");
+            Console.WriteLine(" \t\t\t Earns 1 FREE Bag Of Popcorn. ");
+            Console.WriteLine(" \n\t 3) For Each 3 Boxes of Candy, You Earn A 4th Box Of Candy For Free.\n");
+            
             Tickets tix = new Tickets();
             Concession snacks = new Concession();
-           
 
             tix.GetTickets();
             snacks.GetConcession();
@@ -23,27 +28,26 @@ namespace assignmentFour
             Tickets.TicketCost = Tickets.mTicketCost + Tickets.eTicketCost;
            
             if (Concession.Candy >= 4 || Concession.Candy % 4==0)
-                {
+            {
                 Concession.FreeCandy = (Concession.Candy / 4) * 1.99;
                 Concession.cDiscount = Concession.ConcessionCost - Concession.FreeCandy;
                 Console.WriteLine(" \n\t\tYou Earned "  + Concession.Candy / 4 + " Free Boxes of Candy ");
+            }
+            else
+            {
 
             }
-                else
-                {
-
-                }
-                if (Tickets.eTickets >= 3 && Concession.Popcorn>0)
-                {
-
+                
+            if (Tickets.eTickets >= 3 && Concession.Popcorn>0)
+            {
                 Concession.offPop = 4.50;
                 Concession.pDiscount = Concession.pPrice - Concession.offPop;
                 Console.WriteLine(" \t\tYou Have Earned 1 FREE Popcorn");
+            }
+            else
+            {
+            }
 
-                }
-                else
-                {
-                }
             if (Concession.Popcorn >= 1 && Concession.lDrink >= 1)
             {
                 if (Concession.Popcorn < Concession.lDrink)
@@ -54,20 +58,18 @@ namespace assignmentFour
                 }
                 else
                 {
-                    Tickets.offTix = Concession.lDrink* 2;
+                    Tickets.offTix = Concession.lDrink * 2;
                     Tickets.TixDiscount = Tickets.TicketCost - Tickets.offTix;
                     Console.WriteLine(" \t\tYou Have Earned $" + Tickets.offTix + " OFF Your Ticket(s): ");
                 }
 
             }
-        
             else
             {
             }
 
             double Discount=Concession.FreeCandy + Tickets.offTix + Concession.offPop;
             double TotalCost = ((Tickets.TicketCost + Concession.ConcessionCost)-Discount);
-
 
             Console.WriteLine(" \n\t\t\tTickets: " + Tickets.TicketCost);
             Console.WriteLine( "\t\t\tConcession: " + Concession.ConcessionCost);
