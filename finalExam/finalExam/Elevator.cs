6d2e0a96abc322f5d7b196ebe305bacbd726e543
TierraStover <tierrastover2002@yahoo.com>
2020-04-29 21:27:17 -0400

    added

--- finalExam/finalExam/Elevator.cs
+++ finalExam/finalExam/Elevator.cs
@@ -10,5 +10,22 @@ namespace finalExam
     {
        private double MaxWeight;
        private Passenger[] Occupants;
+       public Elevator(int maxOccupants, double maxWeight)
+        {
+            Passenger[] Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+        public void AddOccupant(Passenger passenger, int index)
+        {
+          Passenger[] Occupants= new Passenger[index];
+        }
+        public double GetCurrentWeight()
+        {
+            return;
+        }
+        public bool IsOverMaxCapacity()
+        {
+            return GetCurrentWeight() > MaxWeight;
+        }
     }
 }
--- finalExam/finalExam/Program.cs
+++ finalExam/finalExam/Program.cs
@@ -10,6 +10,14 @@ namespace finalExam
     {
         static void Main()
         {
+            Passenger p1 = new Passenger("A1", 180);
+            Passenger p2 = new Passenger("A2", 220);
+            Passenger p3 = new Passenger("A1", 200);
+            Passenger p4 = new Passenger("A2", 200);
+            Passenger p5 = new Passenger("A3", 201);
+
+            Elevator elevator1 = new Elevator(2, 400);
+            Elevator elevator2 = new Elevator(3, 600);
         }
     }
 }
